9ea6b9c918a9d0f2ab47ca2374149c0bd6340326
Azad Can Tekin <tekin.act@gmail.com>
2021-10-21 13:51:50 +0300

    Utilities

--- hrms/src/main/java/kodlama/io/hrms/core/utilities/results/SuccessResult.java
+++ hrms/src/main/java/kodlama/io/hrms/core/utilities/results/SuccessResult.java
@@ -1,5 +1,11 @@
 package kodlama.io.hrms.core.utilities.results;
 
-public class SuccessResult {
-
+public class SuccessResult extends Result{
+	public SuccessResult() {
+		super(true);
+	} 
+	
+	public SuccessResult(String message) {
+		super(true,message);
+	} 
 }
