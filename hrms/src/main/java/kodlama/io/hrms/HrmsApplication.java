7488813cbc66b92d26b9634b9a8ca0990f0bb201
Azad Can Tekin <tekin.act@gmail.com>
2021-10-21 13:54:02 +0300

    Swagger2

--- hrms/src/main/java/kodlama/io/hrms/HrmsApplication.java
+++ hrms/src/main/java/kodlama/io/hrms/HrmsApplication.java
@@ -2,6 +2,11 @@ package kodlama.io.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class HrmsApplication {
@@ -9,5 +14,12 @@ public class HrmsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 	}
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("kodlama.io.hrms"))                                     
+          .build();                                           
+    }
 
 }
