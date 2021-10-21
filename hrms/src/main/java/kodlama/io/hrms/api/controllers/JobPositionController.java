e2872541f1de4444ebe021f4c0e10d1c341d17d1
Azad Can Tekin <tekin.act@gmail.com>
2021-10-21 13:55:23 +0300

    Fixed

--- hrms/src/main/java/kodlama/io/hrms/api/controllers/JobPositionController.java
+++ hrms/src/main/java/kodlama/io/hrms/api/controllers/JobPositionController.java
@@ -2,6 +2,7 @@ package kodlama.io.hrms.api.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,8 @@ import kodlama.io.hrms.entities.concretes.JobPosition;
 public class JobPositionController {
 
 	private JobPositionService jobPositionService;
-
+	
+	@Autowired
 	public JobPositionController(JobPositionService jobPositionService) {
 		super();
 		this.jobPositionService = jobPositionService;
