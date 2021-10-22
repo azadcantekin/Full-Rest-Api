3215c8ff80ca664d62757df92e4c8520d873f1e3
Azad Can Tekin <tekin.act@gmail.com>
2021-10-22 15:21:57 +0300

    Commit

--- hrms/src/main/java/kodlama/io/hrms/business/abstracts/JobPositionService.java
+++ hrms/src/main/java/kodlama/io/hrms/business/abstracts/JobPositionService.java
@@ -1,16 +1,14 @@
 package kodlama.io.hrms.business.abstracts;
 
 import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import kodlama.io.hrms.core.utilities.results.DataResult;
+import kodlama.io.hrms.core.utilities.results.Result;
 import kodlama.io.hrms.entities.concretes.JobPosition;
 
 
 public interface JobPositionService {
 
 	DataResult<List<JobPosition>> getall() ;
-
+	Result add (JobPosition jobPosition);
 	
 }
--- hrms/src/main/java/kodlama/io/hrms/business/concretes/JobPositionManager.java
+++ hrms/src/main/java/kodlama/io/hrms/business/concretes/JobPositionManager.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import kodlama.io.hrms.business.abstracts.JobPositionService;
 import kodlama.io.hrms.core.utilities.results.DataResult;
+import kodlama.io.hrms.core.utilities.results.Result;
 import kodlama.io.hrms.core.utilities.results.SuccessDataResult;
+import kodlama.io.hrms.core.utilities.results.SuccessResult;
 import kodlama.io.hrms.dataAccess.abstracts.JobPositionDao;
 import kodlama.io.hrms.entities.concretes.JobPosition;
 
@@ -25,6 +27,12 @@ public class JobPositionManager implements JobPositionService {
 	public DataResult<List<JobPosition>> getall() {
 	
 		return new SuccessDataResult<List<JobPosition>>(this.jobPositionDao.findAll(),"Data has been listed .");
+	}
+
+	@Override
+	public Result add(JobPosition jobPosition) {
+		this.jobPositionDao.save(jobPosition);
+		return new SuccessResult("Position has been added.");
 	}
 
 }
